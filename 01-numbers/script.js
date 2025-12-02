26d92f801be1a58a0a57dd1ed1dd4763aac23af2
angelorapana <angelorapana@icloud.com>
2025-12-02 19:10:08 +0100

    revisione

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -23,9 +23,9 @@ const numThreeMinusNumOne = numThree - numOne;
 console.log(numThreeMinusNumOne); // Output: 24
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = numOne + numTwo + numThree;
+const sum = numOnePlusNumTwo + numThree;
 console.log(sum); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = (numOne + numTwo + numThree) / numOne;
+const numBytes = sum / numOne;
 console.log(numBytes); // Output: 7
